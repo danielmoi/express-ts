7e680b69f7f61adc5409cd33a9abfa9f2408d524
Daniel Moi <dmoixy@gmail.com>
2019-02-01 16:35:03 +1100

    Convert to ts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express Typescript
+
+https://developer.okta.com/blog/2018/11/15/node-express-typescript
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 1337;
+app.get('/', (req, res) => {
+    console.log('HELLO');
+    res.send('Hello world! \n');
+});
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
